1d5d39c6482ee767d59b2e77666ee79b2cbec473
NGUYENNAM <NGUYENNAM@LAPTOP-VL9TJJV6>
2023-06-27 12:14:04 +0700

    verson1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -26,10 +26,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-        public BigInteger s, r, v, decNumm,p, q, g, Y, X;
+        public BigInteger s, r, v, decNumm, p, q, g, Y, X;
 
-        private string inputdata,inputdata1, inputdata2;
-      
+        private string inputdata, inputdata1, inputdata2;
+
         //tính a^exp mod mod
         public static BigInteger power(BigInteger a, BigInteger k, BigInteger q)
         {
@@ -121,39 +121,39 @@ namespace WindowsFormsApp1
         }
 
         private void taokhoa()
-        {  
-                string p1, q1,g1,X1,Y1;
-                p1 = "C667DEF373B2362971945721D8EF0FA5DF1D509C8098EAB51D9B29F4239B99F48B84F0A691B26F54FAEA145DCF1B63F7138EE2917AFEB1393E4297F471CF683BB691C612754D622DF38687644DB046CB712F4FAD01564BB665145421235FF90725FD5213E7764D2074E97DF79D4DF58D1DF7EF6C962B63CA9B0DE231977A9B83";
-                q1 = "98CFCCB04287DDD931CB31314DA0DA8702DF3521";
-                g1 = "52D54BF2A17708C402D52F5A40C7565EE62CA48B62AAE1B9CB1ACE47E3BE228F44EF303F598F97D56E8C3EC9487E3B1F913036030E404672317E8350F8D98D291F30ECE2D916F401914A8C5023D9D88FED209B5BBC0E5E668AB42587C3F0EF4F0BA879BDCC188B97DAC6CE2D1FB8C91F72F0AA9B7AE3C7E10BE0DD5751FE7103";
-                X1 = "014099CE8CE21AA3715531285BE21E09FF49ED8B";
-                Y1 = "52B4721932745ED10A06328FD140F2E5F02BC76E0662700A78CBA28814061C4E38E48CBE593B1C09F1D1FB3B48B579EB05F261AFF268580B7FC433B3E49FFBA84D1DF4FC1F8FC841EB18452754881BDD5ACFEE8BCECA39E798399D466C43B687A6003F989615CD15032B5181C7B6EBE49D92BFE96B2BB8A06A849F1FDAFDC74B";
-                p = BigInteger.Parse(p1, System.Globalization.NumberStyles.HexNumber);
-                g = BigInteger.Parse(g1, System.Globalization.NumberStyles.HexNumber);
-                q = BigInteger.Parse(q1, System.Globalization.NumberStyles.HexNumber);
-                X = BigInteger.Parse(Y1, System.Globalization.NumberStyles.HexNumber);
-                Y = BigInteger.Parse(X1, System.Globalization.NumberStyles.HexNumber);
-                if (p < 0)
-                {
-                    p = -p;
-                }
-                if (q < 0)
-                { 
-                    q=-q;
-                }      
-                if (g < 0)
-                {
-                    g = -g;
-                }
-                if (X < 0) 
-                { 
-                    X = -X;
-                }
-                if (Y < 0) 
-                {
-                    Y = -Y; 
-                }; 
-            
+        {
+            string p1, q1, g1, X1, Y1;
+            p1 = "C667DEF373B2362971945721D8EF0FA5DF1D509C8098EAB51D9B29F4239B99F48B84F0A691B26F54FAEA145DCF1B63F7138EE2917AFEB1393E4297F471CF683BB691C612754D622DF38687644DB046CB712F4FAD01564BB665145421235FF90725FD5213E7764D2074E97DF79D4DF58D1DF7EF6C962B63CA9B0DE231977A9B83";
+            q1 = "98CFCCB04287DDD931CB31314DA0DA8702DF3521";
+            g1 = "52D54BF2A17708C402D52F5A40C7565EE62CA48B62AAE1B9CB1ACE47E3BE228F44EF303F598F97D56E8C3EC9487E3B1F913036030E404672317E8350F8D98D291F30ECE2D916F401914A8C5023D9D88FED209B5BBC0E5E668AB42587C3F0EF4F0BA879BDCC188B97DAC6CE2D1FB8C91F72F0AA9B7AE3C7E10BE0DD5751FE7103";
+            X1 = "014099CE8CE21AA3715531285BE21E09FF49ED8B";
+            Y1 = "52B4721932745ED10A06328FD140F2E5F02BC76E0662700A78CBA28814061C4E38E48CBE593B1C09F1D1FB3B48B579EB05F261AFF268580B7FC433B3E49FFBA84D1DF4FC1F8FC841EB18452754881BDD5ACFEE8BCECA39E798399D466C43B687A6003F989615CD15032B5181C7B6EBE49D92BFE96B2BB8A06A849F1FDAFDC74B";
+            p = BigInteger.Parse(p1, System.Globalization.NumberStyles.HexNumber);
+            g = BigInteger.Parse(g1, System.Globalization.NumberStyles.HexNumber);
+            q = BigInteger.Parse(q1, System.Globalization.NumberStyles.HexNumber);
+            X = BigInteger.Parse(Y1, System.Globalization.NumberStyles.HexNumber);
+            Y = BigInteger.Parse(X1, System.Globalization.NumberStyles.HexNumber);
+            if (p < 0)
+            {
+                p = -p;
+            }
+            if (q < 0)
+            {
+                q = -q;
+            }
+            if (g < 0)
+            {
+                g = -g;
+            }
+            if (X < 0)
+            {
+                X = -X;
+            }
+            if (Y < 0)
+            {
+                Y = -Y;
+            };
+
         }
 
         //reset khoa
@@ -168,56 +168,50 @@ namespace WindowsFormsApp1
 
 
         //ham bam SHA_1
-        public string SHA1hash(string input)
+        public static string SHA1hash(string input)
         {
-            // Chuyển đổi chuỗi đầu vào thành một mảng byte
-            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            // Chuyển đổi chuỗi đầu vào thành mảng byte và lưu độ dài ban đầu
+            byte[] messageBytes = Encoding.UTF8.GetBytes(input);
+            int initialLength = messageBytes.Length;
 
-            // Khởi tạo các biến ban đầu
+            // Thêm các byte padding để độ dài mảng byte chia hết cho 64
+            messageBytes = AddPadding(messageBytes);
+
+            // Tính số block 512-bit từ mảng byte đã được padding
+            int blockCount = messageBytes.Length / 64;
+
+            // Tạo một mảng uint để lưu trữ các từ 32-bit được tạo ra từ các block 512-bit
+            uint[] words = new uint[blockCount * 16];
+
+            // Xử lý từng block 512-bit
+            for (int i = 0; i < blockCount; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    int wordStartIndex = i * 64 + j * 4;
+                    uint word = (uint)messageBytes[wordStartIndex] << 24;
+                    word |= (uint)messageBytes[wordStartIndex + 1] << 16;
+                    word |= (uint)messageBytes[wordStartIndex + 2] << 8;
+                    word |= (uint)messageBytes[wordStartIndex + 3];
+                    words[i * 16 + j] = word;
+                }
+            }
             uint h0 = 0x67452301;
             uint h1 = 0xEFCDAB89;
             uint h2 = 0x98BADCFE;
             uint h3 = 0x10325476;
             uint h4 = 0xC3D2E1F0;
-
-            // Tính toán độ dài của chuỗi đầu vào (theo byte)
-            long messageLength = inputBytes.Length * 8;
-
-            // Thêm bit đầu tiên là 1 và các bit 0 tới khi độ dài của chuỗi mới là một bội số của 512 bit
-            int numberOfZeroBytes = ((448 - inputBytes.Length - 1) % 64 + 64) % 64;
-            byte[] paddedInput = new byte[inputBytes.Length + 1 + numberOfZeroBytes + 8];
-            Array.Copy(inputBytes, 0, paddedInput, 0, inputBytes.Length);
-            paddedInput[inputBytes.Length] = 0x80;
-            for (int i = 0; i < numberOfZeroBytes; i++)
+            for (int i = 0; i < blockCount; i++)
             {
-                paddedInput[inputBytes.Length + 1 + i] = 0x00;
-            }
-
-            // Thêm độ dài của chuỗi đầu vào (theo bit) vào cuối chuỗi
-            byte[] messageLengthBytes = BitConverter.GetBytes((ulong)messageLength);
-            Array.Reverse(messageLengthBytes);
-            Array.Copy(messageLengthBytes, 0, paddedInput, paddedInput.Length - 8, 8);
-
-            // Tính toán các từ của khối tin
-            uint[] words = new uint[paddedInput.Length / 4];
-            for (int i = 0; i < paddedInput.Length / 4; i++)
-            {
-                byte[] temp = new byte[4];
-                Array.Copy(paddedInput, i * 4, temp, 0, 4);
-                Array.Reverse(temp);
-                words[i] = BitConverter.ToUInt32(temp, 0);
-            }
-
-            // Tính toán giá trị băm
-            for (int i = 0; i < words.Length; i += 16)
-            {
-                uint[] block = new uint[80];
-                Array.Copy(words, i, block, 0, 16);
-
-                for (int j = 16; j < 80; j++)
+                // Tạo một mảng 80 từ 32-bit từ 16 từ 32-bit đã được tạo ra từ block 512-bit
+                uint[] w = new uint[80];
+                for (int t = 0; t < 16; t++)
                 {
-                    uint temp = block[j - 3] ^ block[j - 8] ^ block[j - 14] ^ block[j - 16];
-                    block[j] = (temp << 1) | (temp >> 31);
+                    w[t] = words[i * 16 + t];
+                }
+                for (int t = 16; t < 80; t++)
+                {
+                    w[t] = LeftRotate(w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16], 1);
                 }
 
                 uint a = h0;
@@ -225,61 +219,96 @@ namespace WindowsFormsApp1
                 uint c = h2;
                 uint d = h3;
                 uint e = h4;
-
-                for (int j = 0; j < 80; j++)
+                // Xử lý các từ 32-bit trong mảng 80 từ
+                for (int t = 0; t < 80; t++)
                 {
-                    uint f, k;
-                    if (j < 20)
-                    {
-                        f = (b & c) | ((~b) & d);
-                        k = 0x5A827999;
-                    }
-                    else if (j < 40)
-                    {
-                        f = b ^ c ^ d;
-                        k = 0x6ED9EBA1;
-                    }
-                    else if (j < 60)
-                    {
-                        f = (b & c) | (b & d) | (c & d);
-                        k = 0x8F1BBCDC;
-                    }
-                    else
-                    {
-                        f = b ^ c ^ d;
-                        k = 0xCA62C1D6;
-                    }
-
-                    uint temp = ((a << 5) | (a >> 27)) + f + e + k + block[j];
+                    uint temp = LeftRotate(a, 5) + F(t, b, c, d) + e + K(t) + w[t];
                     e = d;
                     d = c;
-                    c = (b << 30) | (b >> 2);
+                    c = LeftRotate(b, 30);
                     b = a;
                     a = temp;
                 }
-
                 h0 += a;
                 h1 += b;
                 h2 += c;
                 h3 += d;
                 h4 += e;
             }
-            // Chuyển đổi giá trị băm từ các biến uint sang chuỗi hex
-            StringBuilder sb = new StringBuilder();
-            sb.Append(h0.ToString("x8"));
-            sb.Append(h1.ToString("x8"));
-            sb.Append(h2.ToString("x8"));
-            sb.Append(h3.ToString("x8"));
-            sb.Append(h4.ToString("x8"));
-            return sb.ToString();
+            // Ghép các giá trị h0, h1, h2, h3, h4 để tạo thành một mảng byte 20-byte
+            byte[] hashBytes = new byte[20];
+            Buffer.BlockCopy(new uint[] { h0, h1, h2, h3, h4 }, 0, hashBytes, 0, 20);
+            // Chuyển đổi giá trị băm từ mảng byte sang chuỗi hex
+            StringBuilder hexBuilder = new StringBuilder(hashBytes.Length * 2);
+            foreach (byte b in hashBytes)
+            {
+                hexBuilder.Append(b.ToString("x2"));
+            }
+            string hashString = hexBuilder.ToString();
+            return hashString;
         }
-        
-        
+        // Hàm hỗ trợ để thêm các byte padding vào mảng byte đầu vào
+        private static byte[] AddPadding(byte[] message)
+        {
+            int messageLength = message.Length;
+            int paddingLength = 64 - ((messageLength + 8) % 64);
+            byte[] paddedMessage = new byte[messageLength + paddingLength + 8];
+            Buffer.BlockCopy(message, 0, paddedMessage, 0, messageLength);
+            paddedMessage[messageLength] = 0x80;
+            long bitLength = (long)messageLength * 8;
+            byte[] bitLengthBytes = BitConverter.GetBytes(bitLength);
+            Buffer.BlockCopy(bitLengthBytes, 0, paddedMessage, paddedMessage.Length - 8, 8);
+            return paddedMessage;
+        }
+        // Hàm hỗ trợ để tính toán giá trị của từ 32-bit trong mảng 80 từ
+        private static uint F(int t, uint b, uint c, uint d)
+        {
+            if (t < 20)
+            {
+                return (b & c) | ((~b) & d);
+            }
+            else if (t < 40)
+            {
+                return b ^ c ^ d;
+            }
+            else if (t < 60)
+            {
+                return (b & c) | (b & d) | (c & d);
+            }
+            else
+            {
+                return b ^ c ^ d;
+            }
+        }
+        // Hàm hỗ trợ để tính toán giá trị của hằng số 32-bit K
+        private static uint K(int t)
+        {
+            if (t < 20)
+            {
+                return 0x5A827999;
+            }
+            else if (t < 40)
+            {
+                return 0x6ED9EBA1;
+            }
+            else if (t < 60)
+            {
+                return 0x8F1BBCDC; }
+            else
+            {
+                return 0xCA62C1D6;
+            }
+        }
+
+        // Hàm hỗ trợ để xoay trái giá trị 32-bit
+        private static uint LeftRotate(uint value, int count)
+        {
+            return (value << count) | (value >> (32 - count));
+        }
+
         //tao chu kí
         private void chuki(string m)
         {
-            taokhoa();
-  
             // Tính giá trị r
             BigInteger k = 37;
             r = BigInteger.ModPow(g, k, p) % q;
@@ -359,20 +388,6 @@ namespace WindowsFormsApp1
             }
             
 
-        }
-        public string SHA1Hash(string input)
-        {
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            using (SHA1Managed sha1 = new SHA1Managed())
-            {
-                byte[] hashBytes = sha1.ComputeHash(inputBytes);
-                StringBuilder hexBuilder = new StringBuilder(hashBytes.Length * 2);
-                foreach (byte b in hashBytes)
-                {
-                    hexBuilder.Append(b.ToString("x2"));
-                }
-                return hexBuilder.ToString();
-            }
         }
         private void button1_Click_1(object sender, EventArgs e)
         {
